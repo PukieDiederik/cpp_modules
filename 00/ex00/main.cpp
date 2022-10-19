1ebba72653601566cc01f65631b3bd075d3e1dd0
Pukiediederik <diederik.boeren@gmail.com>
2022-10-19 01:58:53 +0100

    00/ex00

---
+++ 00/ex00/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	// If this program has no arguments, it prints the following
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	// Loop over each argument
+	for(int i = 1; i < argc; i++)
+	{
+		int	j = 0;
+		// Loop over each letter
+		while (argv[i][j] != '\0')
+			std::cout << (char)toupper(argv[i][j++]);
+	}
+	// Finish with a newline
+	std::cout << std::endl;
+}
