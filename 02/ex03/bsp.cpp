c9a6c4b5fb564d0edbc233dd778f7c93aa46d9fd
Diederik Robert marie boeren <drobert-@c1r17s2.42lisboa.com>
2022-12-07 15:53:44 +0000

    removed comment in BSP

--- 02/ex03/bsp.cpp
+++ 02/ex03/bsp.cpp
@@ -13,12 +13,3 @@ bool bsp(const Point& a, const Point& b, const Point& c, const Point& p)
 	Fixed d = (c.getX() - b.getX()) * (p.getY() - b.getY()) - (c.getY() - b.getY()) * (p.getX() - b.getX());
 	return d == Fixed(0) || (d < Fixed(0)) == (s + t <= Fixed(0));
 }
-
-//var s = (p0.X - p2.X) * (p.Y - p2.Y) - (p0.Y - p2.Y) * (p.X - p2.X);
-//var t = (p1.X - p0.X) * (p.Y - p0.Y) - (p1.Y - p0.Y) * (p.X - p0.X);
-//
-//if ((s < 0) != (t < 0) && s != 0 && t != 0)
-//return false;
-//
-//var d = (p2.X - p1.X) * (p.Y - p1.Y) - (p2.Y - p1.Y) * (p.X - p1.X);
-//return d == 0 || (d < 0) == (s + t <= 0);
